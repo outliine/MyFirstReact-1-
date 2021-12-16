dc5d6c9cf363d052546c692a83f06738942f2c3e
OutLiine <theflash03wade@gmail.com>
2021-12-17 02:06:27 +0800

    Update PageNotFound.js

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -2,7 +2,7 @@ const PageNotFound = () =>{
     return(
         <div>
             <h1>Page Not Found!</h1>
-            <p>Parang siya di mo na makita</p>
+            <p>Quit programming na lods</p>
         </div>
     )
 } 
